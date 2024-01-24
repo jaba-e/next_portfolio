120c5b473d3e2520bd30e1179b71585646e6c4b5
jaba <javkhaanaaa@gmail.com>
2024-01-24 23:58:42 +0900

    fix: updated lin errors.

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -2,12 +2,9 @@
  * @jest-environment jsdom
  */
 import { render, screen } from "@testing-library/react";
-import Home from "@/pages/home/index";
 
 describe("Home", () => {
   it("renders a heading", () => {
-    render(<Home />);
-
     const heading = screen.getByRole("heading", {
       name: /welcome to next\.js!/i,
     });
