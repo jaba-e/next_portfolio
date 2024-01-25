1116037208223131e6981f213211e126255fb4e5
jaba <javkhaanaaa@gmail.com>
2024-01-25 21:48:06 +0900

    feat: don't show divider on last item.

--- components/sections/project.tsx
+++ components/sections/project.tsx
@@ -8,10 +8,10 @@ export default function Project() {
     <>
       <MobileTitle text={"Projects"} />
       <div className="mx-auto max-w-screen-md">
-        {projects.map((project) => (
+        {projects.map((project, i) => (
           <>
             <MediumCard key={project.id} data={project} />
-            <Divider key={project.id} />
+            {i < projects.length - 1 && <Divider key={project.id} />}
           </>
         ))}
       </div>
