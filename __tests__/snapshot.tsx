4dae2b66abbf7b9aa019e357336898573564838e
JABA <javkhaanaaa@gmail.com>
2024-01-25 20:22:21 +0900

    fix: downgraded node version. (#1)
    * fix: downgraded node version.
    
    * feature: implemented github actions
    
    * fix: added yarn resolutions to avoid js compatibility issue.
    
    * fix: added wrap-ansi resolutions as well.
    
    * fix: updated jest test cases.

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { render } from "@testing-library/react";
-import Home from "@/pages/home/index";
+import Home from "@/app/page";
 
 it("renders homepage unchanged", () => {
   const { container } = render(<Home />);
