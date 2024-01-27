4961012728112d1872c3febbd86c3db42a2a4832
jaba <javkhaanaaa@gmail.com>
2024-01-27 23:47:33 +0900

    updated test.

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import { render } from "@testing-library/react";
-import Home from "@/app/page";
+import Home from "../pages";
 
 it("renders homepage unchanged", () => {
   const { container } = render(<Home />);
